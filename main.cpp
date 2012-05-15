c3a715cddb6a8d2d8fb31f9fb0f37d7c7463e1b3
vince4 <vincentmeyer.etu@gmail.com>
2012-05-15 18:48:51 +0200

    Main pour tester la classe Calculateur

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
 #include <QtCore/QCoreApplication>
-#include <iostream>
-#include "Pile.h"
+#include "Calculateur.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    std::cout<<"Hello"<<std::endl;
-
-    Pile p;
-    p.empiler(12.3);
-    p.empiler(14.4);
-    p.empiler(15);
-
-    int f= 45;
-    p.empiler(f);
-
-    Pile p2 = p;
-    p2.empiler(98);
-    p = p2;
-
-    while(!p.pileVide())
-        std::cout<<p.depiler()<<std::endl;
-
+    Calculateur c;
+    c.insererElement("1 3 4+ 2 3 4 5 10+ - * /");
+    c.afficherPile();
     return a.exec();
 }
