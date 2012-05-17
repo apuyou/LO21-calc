93b222004e7d273918bc94536ec8146bc90bf277
vince4 <vincentmeyer.etu@gmail.com>
2012-05-17 19:38:07 +0200

    Ajout du traitement des exceptions et des fonctions sur la pile

--- Calculateur.cpp
+++ Calculateur.cpp
@@ -16,7 +16,7 @@ Calculateur::Calculateur()
 
 void Calculateur::insererElement(const std::string &s)
 {
-    //On test si la pile est vide, dans quel cas, on ne fait rien
+    //On test si l'objet string est vide, dans quel cas, on ne fait rien
     if(s.empty())
         return;
     std::string op; // Contient un opérateur ou une opérande
@@ -51,28 +51,36 @@ void Calculateur::insererElement(const std::string &s)
 
 void Calculateur::addition()
 {
-    //METTRE EN PLACE LA GESTION DES ERREURS : Moins de 2 éléments dans la pile
+    //Gestion des erreurs : on vérifie que la pile contient au moins deux éléments
+    if(pile_m.size() < 2)
+        throw Erreur("La pile ne contient pas assez d'elements");
     if(!pile_m.empty())
         pile_m.push(pile_m.pop() + pile_m.pop());
 }
 
 void Calculateur::soustraction()
 {
-    //METTRE EN PLACE LA GESTION DES ERREURS : Moins de 2 éléments dans la pile
+    //Gestion des erreurs : on vérifie que la pile contient au moins deux éléments
+    if(pile_m.size() < 2)
+        throw Erreur("La pile ne contient pas assez d'elements");
     if(!pile_m.empty())
         pile_m.push(pile_m.pop() - pile_m.pop());
 }
 
 void Calculateur::multiplication()
 {
-    //METTRE EN PLACE LA GESTION DES ERREURS : Moins de 2 éléments dans la pile
+    //Gestion des erreurs : on vérifie que la pile contient au moins deux éléments
+    if(pile_m.size() < 2)
+        throw Erreur("La pile ne contient pas assez d'elements");
     if(!pile_m.empty())
         pile_m.push(pile_m.pop() * pile_m.pop());
 }
 
 void Calculateur::division()
 {
-    //METTRE EN PLACE LA GESTION DES ERREURS : Moins de 2 éléments dans la pile
+    //Gestion des erreurs : on vérifie que la pile contient au moins deux éléments
+    if(pile_m.size() < 2)
+        throw Erreur("La pile ne contient pas assez d'elements");
     if(!pile_m.empty())
         pile_m.push(pile_m.pop() / pile_m.pop());
 }
@@ -83,4 +91,63 @@ void Calculateur::afficherPile(std::ostream &f)
     QStack<float>::iterator it;
     for(it = pile_m.begin(); it < pile_m.end() ; ++it)
         std::cout<<*it<<std::endl;
+}
+
+void Calculateur::swap(int x, int y)
+{
+    //Gestion des erreurs : on vérifie que la pile contient des éléments n°x et n°y
+    if(pile_m.size() <= max(x,y) || x<0 || y<0)
+        throw Erreur("Swap impossible : la pile ne contient pas les elements demandes");
+    //Il faut modifier les valeurs des indices x et y à cause de la représentation de la pile utilisé (sommet en bas)
+    x = pile_m.size()-1 - x;
+    y = pile_m.size()-1 - y;
+    float temp = pile_m[x];
+    pile_m[x] = pile_m[y];
+    pile_m[y] = temp;
+}
+
+void Calculateur::sum(int x)
+{
+    //Gestion des erreurs : on vérifie que la pile contient au moins x éléments
+    if(pile_m.size() < x || x<0)
+        throw Erreur("Sum impossible : la pile ne contient pas assez d'elements pour effectuer la somme");
+    float res(0);
+    for(int i=0; i<x; ++i)
+        res += pile_m.pop();
+    pile_m.push(res);
+}
+
+void Calculateur::mean(int x)
+{
+    //Gestion des erreurs : on vérifie que la pile contient au moins x éléments
+    if(pile_m.size() < x || x<0)
+        throw Erreur("Mean impossible : la pile ne contient pas assez d'elements pour effectuer la moyenne");
+    float res(0);
+    for(int i=0; i<x; ++i)
+        res += pile_m.pop();
+    res /= x;
+    pile_m.push(res);
+}
+
+void Calculateur::clear()
+{
+    pile_m.clear();
+}
+
+void Calculateur::dup()
+{
+    //Gestion des erreurs : Si la pile est vide, on ne fait rien
+    if(pile_m.isEmpty())
+        return;
+    float e(pile_m.pop());
+    pile_m.push(e);
+    pile_m.push(e);
+}
+
+void Calculateur::drop()
+{
+    //Gestion des erreurs : Si la pile est vide, on ne fait rien
+    if(pile_m.isEmpty())
+        return;
+    pile_m.pop();
 }
