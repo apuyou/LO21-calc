72df41ff718f18ad374ae17111e7c4f6f1a1228c
vince4 <vincentmeyer.etu@gmail.com>
2012-05-17 19:41:43 +0200

    Test pour le traitement des exceptions et les fonctions sur la pile

--- main.cpp
+++ main.cpp
@@ -5,7 +5,15 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     Calculateur c;
-    c.insererElement("1 3 4+ 2 3 4 5 10+ - * /");
+    try{
+        c.insererElement("1 1 1 3 4+ 2 3 4 5 10+ + * / ");
+        c.swap(0, 1);
+        c.mean(3);
+        c.drop();
+    }catch(Erreur e){
+        std::cout<<e.what()<<std::endl;
+    }
+
     c.afficherPile();
     return a.exec();
 }
