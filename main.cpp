f350c8ab1a11c3aeb06417a9785033695373b838
vince4 <vincentmeyer.etu@gmail.com>
2012-05-22 18:56:58 +0200

    Fichier de test pour les opérateurs unaires et les types de constantes

--- main.cpp
+++ main.cpp
@@ -6,14 +6,18 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     Calculateur c;
     try{
-        c.insererElement("1 1 1 3 4+ 2 3 4 5 10+ + * / ");
-        c.swap(0, 1);
-        c.mean(3);
-        c.drop();
+        c.insererElement("2 3 3 +");
+        c.modeDegre();
+        c.modeFraction();
+
+        c.modeReel();
+        c.mod();
     }catch(Erreur e){
         std::cout<<e.what()<<std::endl;
     }
-
+    float d;
+    float n = getFraction(0.25, &d);
+    std::cout<<n<<"/"<<d<<std::endl;
     c.afficherPile();
     return a.exec();
 }
