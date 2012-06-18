d72052216cd63eea72d06ee7b615452900246735
Arthur Puyou <arthur@puyou.fr>
2012-06-18 22:21:29 +0200

    Nettoyage du code de qmake

--- onglet.cpp
+++ onglet.cpp
@@ -8,251 +8,157 @@ onglet::onglet(QWidget *parent)
 }
 
 void onglet::setupUi(QWidget *parent){
-   this->setObjectName(QString::fromUtf8("tab"));
    verticalLayout_2 = new QVBoxLayout(this);
-   verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
    horizontalLayout = new QHBoxLayout();
-   horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
    verticalLayout_3 = new QVBoxLayout();
-   verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
-   listWidget = new QListWidget(this);
-   listWidget->setObjectName(QString::fromUtf8("listWidget"));
 
+   listWidget = new QListWidget(this);
    verticalLayout_3->addWidget(listWidget);
 
    inputLine = new QLineEdit(this);
-   inputLine->setObjectName(QString::fromUtf8("inputLine"));
    inputLine->setReadOnly(true);
-
    verticalLayout_3->addWidget(inputLine);
 
-
    horizontalLayout->addLayout(verticalLayout_3);
+
    clavierSimple = new QWidget(this);
 
    gridLayout = new QGridLayout(clavierSimple);
-   gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
-   gridLayout->setSizeConstraint(QLayout::SetDefaultConstraint);
-   enterButton = new QPushButton(this);
-   enterButton->setObjectName(QString::fromUtf8("enterButton"));
 
+   enterButton = new QPushButton("Entree/DUP", this);
    gridLayout->addWidget(enterButton, 7, 0, 1, 3);
 
-   opDivideButton = new QPushButton(this);
-   opDivideButton->setObjectName(QString::fromUtf8("opDivideButton"));
-
+   opDivideButton = new QPushButton("/", this);
    gridLayout->addWidget(opDivideButton, 0, 0, 1, 1);
 
-   deleteButton = new QPushButton(this);
-   deleteButton->setObjectName(QString::fromUtf8("deleteButton"));
-
+   deleteButton = new QPushButton("Del/DROP", this);
    gridLayout->addWidget(deleteButton, 0, 1, 1, 2);
 
-   opPlusButton = new QPushButton(this);
-   opPlusButton->setObjectName(QString::fromUtf8("opPlusButton"));
-
+   opPlusButton = new QPushButton("+", this);
    gridLayout->addWidget(opPlusButton, 1, 0, 1, 1);
 
-   opMinusButton = new QPushButton(this);
-   opMinusButton->setObjectName(QString::fromUtf8("opMinusButton"));
-
+   opMinusButton = new QPushButton("-", this);
    gridLayout->addWidget(opMinusButton, 1, 1, 1, 1);
 
-   opTimesButton = new QPushButton(this);
-   opTimesButton->setObjectName(QString::fromUtf8("opTimesButton"));
-
+   opTimesButton = new QPushButton("*", this);
    gridLayout->addWidget(opTimesButton, 1, 2, 1, 1);
 
-   num7Button = new QPushButton(this);
-   num7Button->setObjectName(QString::fromUtf8("num7Button"));
-
+   num7Button = new QPushButton("7", this);
    gridLayout->addWidget(num7Button, 2, 0, 1, 1);
 
-   num8Button = new QPushButton(this);
-   num8Button->setObjectName(QString::fromUtf8("num8Button"));
-
+   num8Button = new QPushButton("8", this);
    gridLayout->addWidget(num8Button, 2, 1, 1, 1);
 
-   num9Button = new QPushButton(this);
-   num9Button->setObjectName(QString::fromUtf8("num9Button"));
-
+   num9Button = new QPushButton("9", this);
    gridLayout->addWidget(num9Button, 2, 2, 1, 1);
 
-   num4Button = new QPushButton(this);
-   num4Button->setObjectName(QString::fromUtf8("num4Button"));
-
+   num4Button = new QPushButton("4", this);
    gridLayout->addWidget(num4Button, 3, 0, 1, 1);
 
-   num5Button = new QPushButton(this);
-   num5Button->setObjectName(QString::fromUtf8("num5Button"));
-
+   num5Button = new QPushButton("5", this);
    gridLayout->addWidget(num5Button, 3, 1, 1, 1);
 
-   num6Button = new QPushButton(this);
-   num6Button->setObjectName(QString::fromUtf8("num6Button"));
-
+   num6Button = new QPushButton("6", this);
    gridLayout->addWidget(num6Button, 3, 2, 1, 1);
 
-   num1Button = new QPushButton(this);
-   num1Button->setObjectName(QString::fromUtf8("num1Button"));
-
+   num1Button = new QPushButton("1", this);
    gridLayout->addWidget(num1Button, 4, 0, 1, 1);
 
-   num2Button = new QPushButton(this);
-   num2Button->setObjectName(QString::fromUtf8("num2Button"));
-
+   num2Button = new QPushButton("2", this);
    gridLayout->addWidget(num2Button, 4, 1, 1, 1);
 
-   num3Button = new QPushButton(this);
-   num3Button->setObjectName(QString::fromUtf8("num3Button"));
-
+   num3Button = new QPushButton("3", this);
    gridLayout->addWidget(num3Button, 4, 2, 1, 1);
 
-   num0Button = new QPushButton(this);
-   num0Button->setObjectName(QString::fromUtf8("num0Button"));
-
+   num0Button = new QPushButton("0", this);
    gridLayout->addWidget(num0Button, 5, 0, 1, 1);
 
-   quoteButton = new QPushButton(this);
-   quoteButton->setObjectName(QString::fromUtf8("quoteButton"));
-
+   quoteButton = new QPushButton("'", this);
    gridLayout->addWidget(quoteButton, 6, 0, 1, 1);
 
-   spaceButton = new QPushButton(this);
-   spaceButton->setObjectName(QString::fromUtf8("spaceButton"));
-
+   spaceButton = new QPushButton("Espace", this);
    gridLayout->addWidget(spaceButton, 6, 1, 1, 2);
 
-   decimalButton = new QPushButton(this);
-   decimalButton->setObjectName(QString::fromUtf8("decimalButton"));
-   decimalButton->setCheckable(false);
-
+   decimalButton = new QPushButton(".", this);
    gridLayout->addWidget(decimalButton, 5, 1, 1, 1);
 
-   dollarButton = new QPushButton(this);
-   dollarButton->setObjectName(QString::fromUtf8("dollarButton"));
-
+   dollarButton = new QPushButton("$", this);
    gridLayout->addWidget(dollarButton, 5, 2, 1, 1);
-
 
    horizontalLayout->addWidget(clavierSimple);
 
    clavierEtendu = new QWidget(this);
 
    gridLayout_3 = new QGridLayout(clavierEtendu);
-   gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
-   sinButton = new QPushButton(this);
-   sinButton->setObjectName(QString::fromUtf8("sinButton"));
 
+   sinButton = new QPushButton("SIN", this);
    gridLayout_3->addWidget(sinButton, 0, 0, 1, 1);
 
-   cosButton = new QPushButton(this);
-   cosButton->setObjectName(QString::fromUtf8("cosButton"));
-
+   cosButton = new QPushButton("COS", this);
    gridLayout_3->addWidget(cosButton, 1, 0, 1, 1);
 
-   tanButton = new QPushButton(this);
-   tanButton->setObjectName(QString::fromUtf8("tanButton"));
-
+   tanButton = new QPushButton("TAN", this);
    gridLayout_3->addWidget(tanButton, 2, 0, 1, 1);
 
-   sinhButton = new QPushButton(this);
-   sinhButton->setObjectName(QString::fromUtf8("sinhButton"));
-
+   sinhButton = new QPushButton("SINH", this);
    gridLayout_3->addWidget(sinhButton, 0, 1, 1, 1);
 
-   coshButton = new QPushButton(this);
-   coshButton->setObjectName(QString::fromUtf8("coshButton"));
-
+   coshButton = new QPushButton("COSH", this);
    gridLayout_3->addWidget(coshButton, 1, 1, 1, 1);
 
-   tanhButton = new QPushButton(this);
-   tanhButton->setObjectName(QString::fromUtf8("tanhButton"));
-
+   tanhButton = new QPushButton("TANH", this);
    gridLayout_3->addWidget(tanhButton, 2, 1, 1, 1);
 
-   lnButton = new QPushButton(this);
-   lnButton->setObjectName(QString::fromUtf8("lnButton"));
-
+   lnButton = new QPushButton("LN", this);
    gridLayout_3->addWidget(lnButton, 3, 0, 1, 1);
 
-   logButton = new QPushButton(this);
-   logButton->setObjectName(QString::fromUtf8("logButton"));
-
+   logButton = new QPushButton("LOG", this);
    gridLayout_3->addWidget(logButton, 3, 1, 1, 1);
 
-   sqrtButton = new QPushButton(this);
-   sqrtButton->setObjectName(QString::fromUtf8("sqrtButton"));
-
+   sqrtButton = new QPushButton("SQRT", this);
    gridLayout_3->addWidget(sqrtButton, 4, 0, 1, 1);
 
-   sqrButton = new QPushButton(this);
-   sqrButton->setObjectName(QString::fromUtf8("sqrButton"));
-
+   sqrButton = new QPushButton("SQR", this);
    gridLayout_3->addWidget(sqrButton, 4, 1, 1, 1);
 
-   invButton = new QPushButton(this);
-   invButton->setObjectName(QString::fromUtf8("invButton"));
-
+   invButton = new QPushButton("INV", this);
    gridLayout_3->addWidget(invButton, 5, 0, 1, 1);
 
-   cubeButton = new QPushButton(this);
-   cubeButton->setObjectName(QString::fromUtf8("cubeButton"));
-
+   cubeButton = new QPushButton("CUBE", this);
    gridLayout_3->addWidget(cubeButton, 5, 1, 1, 1);
 
-   factButton = new QPushButton(this);
-   factButton->setObjectName(QString::fromUtf8("factButton"));
-
+   factButton = new QPushButton("!", this);
    gridLayout_3->addWidget(factButton, 6, 1, 1, 1);
 
-   clearButton = new QPushButton(this);
-   clearButton->setObjectName(QString::fromUtf8("clearButton"));
-
+   clearButton = new QPushButton("CLEAR", this);
    gridLayout_3->addWidget(clearButton, 7, 1, 1, 1);
 
-   swapButton = new QPushButton(this);
-   swapButton->setObjectName(QString::fromUtf8("swapButton"));
-
+   swapButton = new QPushButton("SWAP", this);
    gridLayout_3->addWidget(swapButton, 7, 0, 1, 1);
 
-   evalButton = new QPushButton(this);
-   evalButton->setObjectName(QString::fromUtf8("evalButton"));
-
+   evalButton = new QPushButton("EVAL", this);
    gridLayout_3->addWidget(evalButton, 6, 0, 1, 1);
 
-
    horizontalLayout->addWidget(clavierEtendu);
-
 
    verticalLayout_2->addLayout(horizontalLayout);
 
    horizontalLayout_3 = new QHBoxLayout();
-   horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
-   comboType = new QComboBox(this);
-   comboType->setObjectName(QString::fromUtf8("comboType"));
 
+   comboType = new QComboBox(this);
    horizontalLayout_3->addWidget(comboType);
 
    comboAngles = new QComboBox(this);
-   comboAngles->setObjectName(QString::fromUtf8("comboAngles"));
-
    horizontalLayout_3->addWidget(comboAngles);
 
    checkboxComplexes = new QCheckBox(this);
-   checkboxComplexes->setObjectName(QString::fromUtf8("checkboxComplexes"));
-
    horizontalLayout_3->addWidget(checkboxComplexes);
 
    horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-
    horizontalLayout_3->addItem(horizontalSpacer);
 
    labelStatus = new QLabel(this);
-   labelStatus->setObjectName(QString::fromUtf8("labelStatus"));
-
    horizontalLayout_3->addWidget(labelStatus);
-
 
    verticalLayout_2->addLayout(horizontalLayout_3);
 
@@ -262,63 +168,29 @@ void onglet::setupUi(QWidget *parent){
 
 void onglet::retranslateUi(QWidget *parent)
 {
-    enterButton->setText(QApplication::translate("Afficheur", "ENTR\303\211E/DUP", 0, QApplication::UnicodeUTF8));
-    enterButton->setShortcut(QApplication::translate("Afficheur", "Return", 0, QApplication::UnicodeUTF8));
-    opDivideButton->setText(QApplication::translate("Afficheur", "/", 0, QApplication::UnicodeUTF8));
-    opDivideButton->setShortcut(QApplication::translate("Afficheur", "/", 0, QApplication::UnicodeUTF8));
-    deleteButton->setText(QApplication::translate("Afficheur", "Del/DROP", 0, QApplication::UnicodeUTF8));
-    deleteButton->setShortcut(QApplication::translate("Afficheur", "Backspace", 0, QApplication::UnicodeUTF8));
-    opPlusButton->setText(QApplication::translate("Afficheur", "+", 0, QApplication::UnicodeUTF8));
-    opPlusButton->setShortcut(QApplication::translate("Afficheur", "+", 0, QApplication::UnicodeUTF8));
-    opMinusButton->setText(QApplication::translate("Afficheur", "-", 0, QApplication::UnicodeUTF8));
-    opMinusButton->setShortcut(QApplication::translate("Afficheur", "-", 0, QApplication::UnicodeUTF8));
-    opTimesButton->setText(QApplication::translate("Afficheur", "*", 0, QApplication::UnicodeUTF8));
-    opTimesButton->setShortcut(QApplication::translate("Afficheur", "*", 0, QApplication::UnicodeUTF8));
-    num7Button->setText(QApplication::translate("Afficheur", "7", 0, QApplication::UnicodeUTF8));
-    num7Button->setShortcut(QApplication::translate("Afficheur", "7", 0, QApplication::UnicodeUTF8));
-    num8Button->setText(QApplication::translate("Afficheur", "8", 0, QApplication::UnicodeUTF8));
-    num8Button->setShortcut(QApplication::translate("Afficheur", "8", 0, QApplication::UnicodeUTF8));
-    num9Button->setText(QApplication::translate("Afficheur", "9", 0, QApplication::UnicodeUTF8));
-    num9Button->setShortcut(QApplication::translate("Afficheur", "9", 0, QApplication::UnicodeUTF8));
-    num4Button->setText(QApplication::translate("Afficheur", "4", 0, QApplication::UnicodeUTF8));
-    num4Button->setShortcut(QApplication::translate("Afficheur", "4", 0, QApplication::UnicodeUTF8));
-    num5Button->setText(QApplication::translate("Afficheur", "5", 0, QApplication::UnicodeUTF8));
-    num5Button->setShortcut(QApplication::translate("Afficheur", "5", 0, QApplication::UnicodeUTF8));
-    num6Button->setText(QApplication::translate("Afficheur", "6", 0, QApplication::UnicodeUTF8));
-    num6Button->setShortcut(QApplication::translate("Afficheur", "6", 0, QApplication::UnicodeUTF8));
-    num1Button->setText(QApplication::translate("Afficheur", "1", 0, QApplication::UnicodeUTF8));
-    num1Button->setShortcut(QApplication::translate("Afficheur", "1", 0, QApplication::UnicodeUTF8));
-    num2Button->setText(QApplication::translate("Afficheur", "2", 0, QApplication::UnicodeUTF8));
-    num2Button->setShortcut(QApplication::translate("Afficheur", "2", 0, QApplication::UnicodeUTF8));
-    num3Button->setText(QApplication::translate("Afficheur", "3", 0, QApplication::UnicodeUTF8));
-    num3Button->setShortcut(QApplication::translate("Afficheur", "3", 0, QApplication::UnicodeUTF8));
-    num0Button->setText(QApplication::translate("Afficheur", "0", 0, QApplication::UnicodeUTF8));
-    num0Button->setShortcut(QApplication::translate("Afficheur", "0", 0, QApplication::UnicodeUTF8));
-    quoteButton->setText(QApplication::translate("Afficheur", "'", 0, QApplication::UnicodeUTF8));
-    quoteButton->setShortcut(QApplication::translate("Afficheur", "'", 0, QApplication::UnicodeUTF8));
-    spaceButton->setText(QApplication::translate("Afficheur", "Espace", 0, QApplication::UnicodeUTF8));
-    spaceButton->setShortcut(QApplication::translate("Afficheur", "Space", 0, QApplication::UnicodeUTF8));
-    decimalButton->setText(QApplication::translate("Afficheur", ".", 0, QApplication::UnicodeUTF8));
-    decimalButton->setShortcut(QApplication::translate("Afficheur", ",", 0, QApplication::UnicodeUTF8));
-    dollarButton->setText(QApplication::translate("Afficheur", "$", 0, QApplication::UnicodeUTF8));
-    dollarButton->setShortcut(QApplication::translate("Afficheur", "$", 0, QApplication::UnicodeUTF8));
-    sinButton->setText(QApplication::translate("Afficheur", "SIN", 0, QApplication::UnicodeUTF8));
-    cosButton->setText(QApplication::translate("Afficheur", "COS", 0, QApplication::UnicodeUTF8));
-    tanButton->setText(QApplication::translate("Afficheur", "TAN", 0, QApplication::UnicodeUTF8));
-    sinhButton->setText(QApplication::translate("Afficheur", "SINH", 0, QApplication::UnicodeUTF8));
-    coshButton->setText(QApplication::translate("Afficheur", "COSH", 0, QApplication::UnicodeUTF8));
-    tanhButton->setText(QApplication::translate("Afficheur", "TANH", 0, QApplication::UnicodeUTF8));
-    lnButton->setText(QApplication::translate("Afficheur", "LN", 0, QApplication::UnicodeUTF8));
-    logButton->setText(QApplication::translate("Afficheur", "LOG", 0, QApplication::UnicodeUTF8));
-    sqrtButton->setText(QApplication::translate("Afficheur", "SQRT", 0, QApplication::UnicodeUTF8));
-    sqrButton->setText(QApplication::translate("Afficheur", "SQR", 0, QApplication::UnicodeUTF8));
-    invButton->setText(QApplication::translate("Afficheur", "INV", 0, QApplication::UnicodeUTF8));
-    cubeButton->setText(QApplication::translate("Afficheur", "CUBE", 0, QApplication::UnicodeUTF8));
-    factButton->setText(QApplication::translate("Afficheur", "!", 0, QApplication::UnicodeUTF8));
-    clearButton->setText(QApplication::translate("Afficheur", "CLEAR", 0, QApplication::UnicodeUTF8));
-    clearButton->setShortcut(QApplication::translate("Afficheur", "CTRL+C", 0, QApplication::UnicodeUTF8));
-    swapButton->setText(QApplication::translate("Afficheur", "SWAP", 0, QApplication::UnicodeUTF8));
-    evalButton->setText(QApplication::translate("Afficheur", "EVAL", 0, QApplication::UnicodeUTF8));
+    enterButton->setShortcut(QString("Return"));
+    opDivideButton->setShortcut(QString("/"));
+    deleteButton->setShortcut(QString("Backspace"));
+    opPlusButton->setShortcut(QString("+"));
+    opMinusButton->setShortcut(QString("-"));
+    opTimesButton->setShortcut(QString("*"));
+    num7Button->setShortcut(QString("7"));
+    num8Button->setShortcut(QString("8"));
+    num9Button->setShortcut(QString("9"));
+    num4Button->setShortcut(QString("4"));
+    num5Button->setShortcut(QString("5"));
+    num6Button->setShortcut(QString("6"));
+    num1Button->setShortcut(QString("1"));
+    num2Button->setShortcut(QString("2"));
+    num3Button->setShortcut(QString("3"));
+    num0Button->setShortcut(QString("0"));
+    quoteButton->setShortcut(QString("'"));
+    spaceButton->setShortcut(QString("Space"));
+    decimalButton->setShortcut(QString(","));
+    dollarButton->setShortcut(QString("$"));
+    clearButton->setShortcut(QString("CTRL+C"));
+
+
     comboType->clear();
     comboType->insertItems(0, QStringList()
      << QApplication::translate("Afficheur", "Entier", 0, QApplication::UnicodeUTF8)
@@ -328,6 +200,7 @@ void onglet::retranslateUi(QWidget *parent)
 #ifndef QT_NO_TOOLTIP
     comboType->setToolTip(QApplication::translate("Afficheur", "Type de constantes", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
+
     comboAngles->clear();
     comboAngles->insertItems(0, QStringList()
      << QApplication::translate("Afficheur", "Degr\303\251s", 0, QApplication::UnicodeUTF8)
@@ -336,9 +209,11 @@ void onglet::retranslateUi(QWidget *parent)
 #ifndef QT_NO_TOOLTIP
     comboAngles->setToolTip(QApplication::translate("Afficheur", "Format des angles", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
+
     checkboxComplexes->setText(QApplication::translate("Afficheur", "&Complexes", 0, QApplication::UnicodeUTF8));
+
     labelStatus->setText(QString());
-} // retranslateUi
+}
 
 void onglet::setupSignals(QWidget *parent){
     // Pavé numérique
