026444bd06f60ab20886d2dd6c69d2b37e7c444c
Arthur Puyou <arthur@puyou.fr>
2012-06-18 03:14:49 +0200

    Titre de la fenêtre principale

--- afficheur.cpp
+++ afficheur.cpp
@@ -1,3 +1,15 @@
+/**
+ * \file afficheur.cpp
+ * \brief Contient les méthodes de la classe Afficheur
+ * \author Vincent Meyer et Arthur Puyou
+ * \version 0.1
+ * \date 15 Mai 2012
+ *
+ * La classe Afficheur sert à afficher les données de la classe Calculateur. Elle gère
+ * l'affichage de la fenêtre principale et hérite donc de QMainWindow.
+ *
+ */
+
 #include "afficheur.h"
 #include "ui_afficheur.h"
 #include "Calculateur.h"
@@ -9,6 +21,8 @@ Afficheur::Afficheur(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    setWindowTitle(tr("calcul'utc"));
+
     // Menus
     connect(ui->actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
 
@@ -18,7 +32,6 @@ Afficheur::Afficheur(QWidget *parent) :
     clavierGroup->addAction(ui->actionClavier_etendu);
 
     ui->tabWidget->addTab(new onglet(this), QString("Onglet 1"));
-    ui->tabWidget->addTab(new onglet(this), QString("Onglet 2"));
     ui->tabWidget->setCurrentIndex(0);
 
 }
